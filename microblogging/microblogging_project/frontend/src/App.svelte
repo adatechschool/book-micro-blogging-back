d5a27d488966a9a03aa34d77df611adb9899559c
Arnaud <hayon.arnaud@gmail.com>
2024-12-05 12:11:31 +0100

    setup: installation vite svelte and tailwind in the back environment

---
+++ microblogging/microblogging_project/frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import TailwindCSS from "./TailwindCSS.svelte";
+</script>
+
+<TailwindCSS />
+
+<main>
+  <h1 class="bg-slate-50">Coucou bisous</h1>
+</main>
+
+
---
+++ microblogging/microblogging_project/views.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from .supabase_utils import fetch_from_supabase, insert_to_supabase
+from django.shortcuts import render
+
+def fetch_data_view(request):
+    data = fetch_from_supabase('users')
+    return JsonResponse(data, safe=False)
+
+def insert_data_view(request):
+    if request.method == 'POST':
+        data = request.POST.dict()
+        response = insert_to_supabase('users', data)
+        return JsonResponse(response, safe=False)
+    
+def index(request):
+    return render(request, 'index.html')
