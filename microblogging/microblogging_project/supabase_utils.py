5746bd6281377171629a6974b3cced5571653758
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-12-09 12:03:56 +0100

    add: manage try except on insertion request

--- microblogging/microblogging_project/supabase_utils.py
+++ microblogging/microblogging_project/supabase_utils.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from django.http import JsonResponse
 from django.conf import settings
 
 SUPABASE_URL = settings.SUPABASE_URL
@@ -17,5 +19,13 @@ def fetch_from_supabase(endpoint):
 
 def insert_to_supabase(endpoint, data):
     url = f"{SUPABASE_URL}/rest/v1/{endpoint}"
-    response = requests.post(url, json=data, headers=headers)
-    return response.json()
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        if response.status_code == 201:
+            print(f"Success: {response.status_code} - {response.text}")
+            return True
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return False
+    except:
+        return False
--- microblogging/users_app/views.py
+++ microblogging/users_app/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from . import templates
+from django.http import JsonResponse, HttpResponse
+from users_app import templates
+from django.views.decorators.csrf import csrf_exempt
 from microblogging_project.supabase_utils import fetch_from_supabase, insert_to_supabase
-from users_app.models import User
+from users_app.models import User, Post
+import json
+
+# def index(request):
+#     users = User.objects.all().values()
+#     context = {
+#         'users': users,
+#     } 
+#     print(f"🦄 {users}")
+#     return render(request, 'first_template.html', context)
 
 def index(request):
-    users = User.objects.all().values()
+    posts = Post.objects.all()
     context = {
-        'users': users,
+        'posts': posts,
     } 
+    print(f"🦄 {posts}")
     return render(request, 'first_template.html', context)
 
 def fetch_users(request):
-    data = fetch_from_supabase('users')
+    data = fetch_from_supabase('users_app_user')
     return JsonResponse(data, safe=False)
 
+@csrf_exempt
 def insert_user(request):
     if request.method == 'POST':
-        data = request.POST.dict()
-        response = insert_to_supabase('users', data)
-        return JsonResponse(response, safe=False)
+        # data = request.POST.dict()
+        data = json.loads(request.body)
+        print(f"🐯 {data}")
+        response = insert_to_supabase('users_app_user', data)
+        if response == True:
+            return JsonResponse({
+                "result": "✅ L'utilisateur a bien été créé."
+            }, status=201)
+        else:
+            return JsonResponse({
+                "result": "⛔️ L'utilisateur (email et/ou username) existe déjà."
+            }, status=403)
