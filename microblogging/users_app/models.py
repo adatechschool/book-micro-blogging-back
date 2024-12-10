7cae95bbe6553c3c81a3264e99236a33b510ff8a
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-12-10 18:25:15 +0100

    fix: import UserAuth to get bio info

--- microblogging/users_app/models.py
+++ microblogging/users_app/models.py
@@ -13,6 +13,26 @@ from django.contrib.auth.models import User
 #     def __str__(self):
 #         return self.username
 
+# class User(models.Model):
+#     bio = models.TextField(max_length=500)
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    bio = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
 class Follower(models.Model):
     follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
     followed = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
