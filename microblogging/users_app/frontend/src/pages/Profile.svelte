ab10ccca0877a8487a84b683b6f1f7228d80f42c
Arnaud <hayon.arnaud@gmail.com>
2024-12-12 16:16:21 +0100

    update: logout button on profile page

--- microblogging/users_app/frontend/src/pages/Profile.svelte
+++ microblogging/users_app/frontend/src/pages/Profile.svelte
@@ -21,7 +21,6 @@
     let likedPosts = new Set();
     
     const toggleLike = (postId) => { 
-        // Basculer le like pour ce post spécifique
         if (likedPosts.has(postId)) {
             likedPosts.delete(postId);
         } else {
@@ -30,18 +29,24 @@
         // Forcer une mise à jour de Svelte
         posts = [...posts];
     } 
+
+    const lougOutClick = () => { 
+      window.location.href = '/accounts/logout/';
+    }
     
   </script>
   
   <TailwindCSS />
 
-  <main class="min-h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% pb-40">
+  <main class=" w-full min-h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% pb-40">
     <Header />
     <br>
-    <user>
-      <top>
-      @{user.username}<br>
-      <button>Se déconnecter</button>
+    <user class="max-w-[100vh] h-screen px-4 space-y-4 pb-16">
+      <top class=" flex justify-between p-4 font-bold">
+        <div class="text-2xl">@{user.username}</div>
+      
+      <button on:click={lougOutClick} type="button" id="logout" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Se déconnecter</button>
+
       </top>
       <bio>
         <p>Bio: {user.bio || 'No bio available'}</p>      </bio>
